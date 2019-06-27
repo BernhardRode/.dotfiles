b8b272de358328a13c9a35b19cbce59d61bf28c8
Bernhard Rode <mail@bernhardrode.de>
2019-06-27 09:20:13 +0200

    feat: rm dsstore and clenaup some zsh stuff

--- zsh/custom/path.zsh
+++ zsh/custom/path.zsh
@@ -2,9 +2,3 @@ export PATH=$PATH:/usr/local/bin
 export PATH=$PATH:$HOME/bin
 export PATH=$PATH:$HOME/Applications/dotfiles/scripts
 export PATH="$PATH:$HOME/Development/local-development/bin"
-export PATH="$PATH:$HOME/Applications/istio/latest/bin"
-
-export CPPFLAGS=-I/usr/local/opt/openssl/include
-export LDFLAGS=-L/usr/local/opt/openssl/lib
-
-source $HOME/.cargo/env
