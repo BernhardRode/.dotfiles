6c3c42db7f03de587534ad965c3d3663d3e8a943
Bernhard Rode <rbo2abt@bosch.com>
2024-04-09 14:08:28 +0200

    feat: use gh for auth

--- config/nvim/lua/ebbo.lua
+++ config/nvim/lua/ebbo.lua
@@ -17,3 +17,10 @@ local config = {
     " ",
   },
 }
+
+vim.keymap.set('i', '<C-J>', 'copilot#Accept("\\<CR>")', {
+  expr = true,
+  replace_keycodes = false
+})
+vim.g.copilot_no_tab_map = true
+
