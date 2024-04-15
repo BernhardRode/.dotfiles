5fb8e4a52be839578371463afcbfaa0c60c26b64
Bernhard Rode <rbo2abt@bosch.com>
2024-04-15 20:56:20 +0200

    wip: minor bugfixes and improvements

--- config/nvim/lua/bernhardrode/plugins/telescope.lua
+++ config/nvim/lua/bernhardrode/plugins/telescope.lua
@@ -10,6 +10,7 @@ return {
   config = function()
     local telescope = require("telescope")
     local actions = require("telescope.actions")
+    local builtin = require("telescope.builtin")
     local transform_mod = require("telescope.actions.mt").transform_mod
 
     local trouble = require("trouble")
@@ -41,10 +42,12 @@ return {
     -- set keymaps
     local keymap = vim.keymap -- for conciseness
 
-    keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
-    keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
-    keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
-    keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
-    keymap.set("n", "<leader>ft", "<cmd>TodoTelescope<cr>", { desc = "Find todos" })
+    keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "[f]ind [r]ecent files" })
+    keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "[f]ind [f]iles in cwd" })
+    keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "[f]ind [s]string in cwd" })
+    keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "[f]ind [s]tring under cursor in cwd" })
+    keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<cr>", { desc = "[f]ind [h]elp" })
+    keymap.set("n", "<leader>fh", "<cmd>Telescope diagnostics<cr>", { desc = "[f]ind [d]iagnostics" })
+    keymap.set("n", "<leader>ft", "<cmd>TodoTelescope<cr>", { desc = "[f]ind [t]odos" })
   end,
 }
