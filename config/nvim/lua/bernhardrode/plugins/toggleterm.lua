5a86e8c50f95f7261d13ea59865cafac68535764
Bernhard Rode <rbo2abt@bosch.com>
2024-04-17 17:40:03 +0200

    wip: minor bugfixes and improvements

--- config/nvim/lua/bernhardrode/plugins/toggleterm.lua
+++ config/nvim/lua/bernhardrode/plugins/toggleterm.lua
@@ -1,1 +1,16 @@
-return { "akinsho/toggleterm.nvim", version = "*", config = true }
+return {
+  "akinsho/toggleterm.nvim",
+
+  -- keys = {
+  --   { "<leader>c", "<cmd>ToggleTerm direction=float dir=${vim.loop.dir}<cr>", desc = "ToggleTerm open" },
+  -- },
+  config = function()
+    require("toggleterm").setup({
+      autochdir = true,
+      direction = "float",
+      insert_mappings = true,
+      open_mapping = [[<c-c>]],
+      terminal_mappings = true,
+    })
+  end,
+}
