042f80cff7aeeb66570556249d4c0dca16eb1daf
Bernhard Rode <rbo2abt@bosch.com>
2024-04-30 17:28:26 +0200

    wip: minor bugfixes and improvements

--- config/shell/settings.sh
+++ config/shell/settings.sh
@@ -1,2 +1,3 @@
 export EDITOR=nvim
 export XDG_CONFIG_HOME="$HOME/.config"
+export GPG_TTY=$(tty)
--- config/zsh/plugins-zcomet.zsh
+++ config/zsh/plugins-zcomet.zsh
@@ -1,8 +1,8 @@
 # Clone zcomet if necessary
-if [[ ! -f ${ZDOTDIR:-${HOME}}/.zcomet/bin/zcomet.zsh ]]; then
-  command git clone https://github.com/agkozak/zcomet.git ${ZDOTDIR:-${HOME}}/.zcomet/bin
-fi
-
+# if [[ ! -f ${ZDOTDIR:-${HOME}}/.zcomet/bin/zcomet.zsh ]]; then
+#   command git clone https://github.com/agkozak/zcomet.git ${ZDOTDIR:-${HOME}}/.zcomet/bin
+# fi
+#
 source ${ZDOTDIR:-${HOME}}/.zcomet/bin/zcomet.zsh
 
 zcomet load spaceship-prompt/spaceship-prompt
