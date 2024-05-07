86cdeec3c479ac845eab4b4c3cc88f2c36f8e30e
Bernhard Rode <mail@bernhardrode.de>
2024-05-07 21:41:49 +0200

    wip: minor bugfixes and improvements

--- config/shell/external.sh
+++ config/shell/external.sh
@@ -1,3 +1,15 @@
-# export NVM_DIR=~/.nvm
-# [[ -s "$NVM_DIR/nvm.sh" ]] && source "$NVM_DIR/nvm.sh"
-# [ -s "$NVM_DIR/bash_completion" ] && source "$NVM_DIR/bash_completion"
+if [ -f ~/.cargo/env ]; then
+  . "$HOME/.cargo/env"
+else
+  echo "Rust is not installed. Install it with the following command:"
+  echo "curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh"
+fi
+
+export NVM_DIR="$HOME/.nvm"
+if [ -d $NVM_DIR ]; then
+  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+  # [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+else
+  echo "NVM is not installed. Install it with the following command:"
+  echo "curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash"
+fi
