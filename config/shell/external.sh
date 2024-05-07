3e3c10fac31bc770f37f8561eac91d579f99c91e
Bernhard Rode <mail@bernhardrode.de>
2024-05-07 22:04:42 +0200

    wip: minor bugfixes and improvements

--- config/shell/external.sh
+++ config/shell/external.sh
@@ -1,15 +1,17 @@
-if [ -f ~/.cargo/env ]; then
-  . "$HOME/.cargo/env"
-else
-  echo "Rust is not installed. Install it with the following command:"
-  echo "curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh"
-fi
+# if [ -f ~/.cargo/env ]; then
+#   . "$HOME/.cargo/env"
+# else
+#   echo "Rust is not installed. Install it with the following command:"
+#   echo "curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh"
+# fi
+#
+# export NVM_DIR="$HOME/.nvm"
+# if [ -d $NVM_DIR ]; then
+#   [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+#   # [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+# else
+#   echo "NVM is not installed. Install it with the following command:"
+#   echo "curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash"
+# fi
 
-export NVM_DIR="$HOME/.nvm"
-if [ -d $NVM_DIR ]; then
-  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-  # [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-else
-  echo "NVM is not installed. Install it with the following command:"
-  echo "curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash"
-fi
+
--- config/zsh/settings.zsh
+++ config/zsh/settings.zsh
@@ -6,3 +6,34 @@ eval "$(direnv hook zsh)"
 bindkey "^[[1;3C" forward-word
 bindkey "^[[1;3D" backward-word
 
+timezsh() {
+  shell=${1-$SHELL}
+  for i in $(seq 1 10); do
+    /usr/bin/time $shell -i -c exit
+  done
+}
+
+load-nvmrc() {
+  local node_version="$(nvm version)"
+  local nvmrc_path="$(nvm_find_nvmrc)"
+
+  if [ -n "$nvmrc_path" ]; then
+    local nvmrc_node_version=$(nvm version "$(cat "${nvmrc_path}")")
+
+    if [ "$nvmrc_node_version" = "N/A" ]; then
+      nvm install
+    elif [ "$nvmrc_node_version" != "$node_version" ]; then
+      nvm use
+    fi
+  elif [ "$node_version" != "$(nvm version default)" ]; then
+    nvm use default
+  fi
+}
+
+if [ -s "$HOME/.nvm/nvm.sh" ]; then
+  export NVM_DIR="$HOME/.nvm"
+  nvm_cmds=(nvm node npm yarn)
+  for cmd in $nvm_cmds ; do
+    alias $cmd="unalias $nvm_cmds && unset nvm_cmds && . $NVM_DIR/nvm.sh --no-use && load-nvmrc && $cmd"
+  done
+fi
